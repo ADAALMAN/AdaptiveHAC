b4a640c7e6ca4b3e52882ddadfca77d89fae271d
ADAALMAN <Adaalman@outlook.com>
2024-03-27 21:24:47 +0100

    Update main.py

--- AdaptiveHAC/main.py
+++ AdaptiveHAC/main.py
@@ -18,15 +18,13 @@ eng = matlab.engine.start_matlab()
 def load_data(path, file_name = None):
     # load data file with the matlab engine and unpack data
     if file_name != None:
-        mat = sci.loadmat(f'{path}{file_name}.mat')
-        data = mat['hil_resha_aligned']
-        lbl = mat['lbl_out']
+        mat = eng.load(f'{path}{file_name}.mat')
+        data = np.asarray(mat['hil_resha_aligned'], dtype=np.complex64) # reduce data in memory
+        lbl = np.asarray(mat['lbl_out'])
 
         # resize the labels to the data length
         if lbl.shape[1] > data.shape[1]:
             lbl = np.array(lbl)[:,:data.shape[1]]
-
-        data = np.asarray(data, dtype=np.complex64) # reduce data in memory
 
     return data, lbl
 

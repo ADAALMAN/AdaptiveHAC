0e46918bb00832600bc67944dee7184ef32a1ddd
ADAALMAN <Adaalman@outlook.com>
2024-04-25 00:16:27 +0200

    split files

---
+++ AdaptiveHAC/lib/process.py
@@ -0,0 +1,98 @@
+import numpy as np
+import os, sys, hydra, omegaconf, yaml, argparse, logging, gc
+import scipy.io as sci
+from tqdm import tqdm
+from memory_profiler import memory_usage
+from AdaptiveHAC.lib import timing_decorator, load_data
+from AdaptiveHAC.segmentation import segmentation
+from AdaptiveHAC.processing import PC_processing
+logger = logging.getLogger(__name__) 
+
+def log_memory_usage(logger):
+    """Logs the current memory usage."""
+    mem_usage = memory_usage(-1, interval=0.1, timeout=1)  # Get current memory usage
+    logger.info(f"Current memory usage: {mem_usage[0]} MiB")
+    
+def process(args, file_name):
+    omegaconf.OmegaConf.set_struct(args, False)
+    data_path = hydra.utils.to_absolute_path(args.data_path)
+    log_memory_usage(logger)
+    match args.node_method:
+        case "all":
+            data, lbl = load_data.load_data(data_path, file_name)
+        case "individual":
+            return # depreciated
+            i = 0
+            data, lbl = load_data.load_data(data_path, file_name)
+            data = data[:,:, i]
+        case _ if isinstance(args.node_method, int):
+            data, lbl = load_data.load_data(data_path, file_name)
+            data = data[:,:, args.node_method]
+    log_memory_usage(logger)
+    match args.sample_method:
+        case "windowing":
+            seg_th = "NA"
+            if isinstance(args.node_method, int):
+                samples, labels = PC_processing.SNsample(data, lbl, sample_size = 'default')
+            else:
+                samples, labels = PC_processing.sample(data, lbl, sample_size = 'default')
+        case "segmentation":
+            seg_th = 100
+            segmentation_eng = segmentation.init_matlab(args.root)
+            if isinstance(args.node_method, int):
+                samples, labels, H_avg_score, entropy, PBC = segmentation.SNsegmentation(data, lbl, segmentation_eng, args.root)
+            else:
+                samples, labels, H_avg_score, entropy, PBC = segmentation.segmentation(data, lbl, segmentation_eng, args.root)
+            samples, labels = segmentation.segmentation_thresholding(samples, labels, seg_th, "split")
+            
+    data_len = data.shape[1]  
+    log_memory_usage(logger)      
+    del data, lbl
+    gc.collect()
+    log_memory_usage(logger)
+    features = {}
+    for feature in args.features:
+        match feature:
+            case "none":
+                pass
+            case "entropy":
+                entropies = []
+                j = 0
+                for i in range(len(samples)):
+                    entropies.append(np.mean(entropy[int(j/data_len*len(entropy)):
+                                                     int((j+samples[i].shape[1])/data_len*len(entropy))]))
+                    j = j + samples[i].shape[1]
+                features["entropy"] = entropies
+            case "PBC":
+                PBCs = []
+                j = 0
+                for i in range(len(samples)):
+                    PBCs.append(np.mean(PBC[int(j/data_len*len(PBC)):
+                                                     int((j+samples[i].shape[1])/data_len*len(PBC))]))
+                    j = j + samples[i].shape[1]
+                features["PBC"] = PBCs
+            case "time":
+                features["time"] = "sequence-based"
+        
+    npoints = 1024
+    thr = 0.8
+    match args.subsegmentation:
+        case "fixed-amount":
+            param = 6 # amount of subsegments
+            processing_eng = PC_processing.init_matlab(args.root)
+            if isinstance(args.node_method, int):
+                samples_PC = PC_processing.SNPC_generation(samples, args.subsegmentation, param, npoints, thr, features, labels, processing_eng)
+            else:
+                samples_PC = PC_processing.PC_generation(samples, args.subsegmentation, param, npoints, thr, features, labels, processing_eng)
+        case "fixed-length":
+            param = 20 # subsegment length
+            processing_eng = PC_processing.init_matlab(args.root)
+            if isinstance(args.node_method, int):
+                samples_PC = PC_processing.SNPC_generation(samples, args.subsegmentation, param, npoints, thr, features, labels, processing_eng)
+            else:
+                samples_PC = PC_processing.PC_generation(samples, args.subsegmentation, param, npoints, thr, features, labels, processing_eng)        
+    log_memory_usage(logger)
+    del samples, labels
+    gc.collect()
+    log_memory_usage(logger)
+    return #samples_PC
--- AdaptiveHAC/main.py
+++ AdaptiveHAC/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os, sys, hydra, omegaconf, yaml, argparse, logging, gc
 from AdaptiveHAC.pointTransformer import train_cls, point_transformer
-from AdaptiveHAC.lib import timing_decorator
+from AdaptiveHAC.lib import timing_decorator, load_data, process
 from AdaptiveHAC.segmentation import segmentation
 from AdaptiveHAC.processing import PC_processing
 import scipy.io as sci
@@ -11,19 +11,6 @@ np.set_printoptions(threshold=sys.maxsize)
 
 # initialize matlab
 os.environ['HYDRA_FULL_ERROR'] = '1'
-
-def load_data(path, file_name = None):
-    # load data file with the matlab engine and unpack data
-    if file_name != None:
-        mat = sci.loadmat(f'{path}/{file_name}')
-        data = np.asarray(mat['hil_resha_aligned'], dtype=np.complex64) # reduce data in memory
-        lbl = np.asarray(mat['lbl_out'])
-
-        # resize the labels to the data length
-        if lbl.shape[1] > data.shape[1]:
-            lbl = np.array(lbl)[:,:data.shape[1]]
-
-    return data, lbl   
             
 def load_PT_config(PT_config_path):
     with open(f'{PT_config_path}cls.yaml', 'r') as file:
@@ -42,103 +29,27 @@ def load_PT_config(PT_config_path):
     dict_args.experiment_folder = './'
     return dict_args
     
-def process(args, file_name):
-    omegaconf.OmegaConf.set_struct(args, False)
-    data_path = hydra.utils.to_absolute_path(args.data_path)
-    
-    match args.node_method:
-        case "all":
-            data, lbl = load_data(data_path, file_name)
-        case "individual":
-            return # depreciated
-            i = 0
-            data, lbl = load_data(data_path, file_name)
-            data = data[:,:, i]
-        case _ if isinstance(args.node_method, int):
-            data, lbl = load_data(data_path, file_name)
-            data = data[:,:, args.node_method]
-    
-    match args.sample_method:
-        case "windowing":
-            seg_th = "NA"
-            if isinstance(args.node_method, int):
-                samples, labels = PC_processing.SNsample(data, lbl, sample_size = 'default')
-            else:
-                samples, labels = PC_processing.sample(data, lbl, sample_size = 'default')
-        case "segmentation":
-            seg_th = 100
-            segmentation_eng = segmentation.init_matlab(args.root)
-            if isinstance(args.node_method, int):
-                samples, labels, H_avg_score, entropy, PBC = segmentation.SNsegmentation(data, lbl, segmentation_eng, args.root)
-            else:
-                samples, labels, H_avg_score, entropy, PBC = segmentation.segmentation(data, lbl, segmentation_eng, args.root)
-            samples, labels = segmentation.segmentation_thresholding(samples, labels, seg_th, "split")
-            
-    data_len = data.shape[1]        
-    del(data, lbl)
-    
-    features = {}
-    for feature in args.features:
-        match feature:
-            case "none":
-                pass
-            case "entropy":
-                entropies = []
-                j = 0
-                for i in range(len(samples)):
-                    entropies.append(np.mean(entropy[int(j/data_len*len(entropy)):
-                                                     int((j+samples[i].shape[1])/data_len*len(entropy))]))
-                    j = j + samples[i].shape[1]
-                features["entropy"] = entropies
-            case "PBC":
-                PBCs = []
-                j = 0
-                for i in range(len(samples)):
-                    PBCs.append(np.mean(PBC[int(j/data_len*len(PBC)):
-                                                     int((j+samples[i].shape[1])/data_len*len(PBC))]))
-                    j = j + samples[i].shape[1]
-                features["PBC"] = PBCs
-            case "time":
-                features["time"] = "sequence-based"
-        
-    npoints = 1024
-    thr = 0.8
-    match args.subsegmentation:
-        case "fixed-amount":
-            param = 6 # amount of subsegments
-            processing_eng = PC_processing.init_matlab(args.root)
-            if isinstance(args.node_method, int):
-                samples_PC = PC_processing.SNPC_generation(samples, args.subsegmentation, param, npoints, thr, features, labels, processing_eng)
-            else:
-                samples_PC = PC_processing.PC_generation(samples, args.subsegmentation, param, npoints, thr, features, labels, processing_eng)
-        case "fixed-length":
-            param = 20 # subsegment length
-            processing_eng = PC_processing.init_matlab(args.root)
-            if isinstance(args.node_method, int):
-                samples_PC = PC_processing.SNPC_generation(samples, args.subsegmentation, param, npoints, thr, features, labels, processing_eng)
-            else:
-                samples_PC = PC_processing.PC_generation(samples, args.subsegmentation, param, npoints, thr, features, labels, processing_eng)        
-    
-    return samples_PC
-
 def log_memory_usage(logger):
     """Logs the current memory usage."""
     mem_usage = memory_usage(-1, interval=0.1, timeout=1)  # Get current memory usage
     logger.info(f"Current memory usage: {mem_usage[0]} MiB")
-    
+logger = logging.getLogger(__name__)    
+
 @hydra.main(config_path="conf", config_name="paramsweep", version_base='1.3')
 def main(args):
     omegaconf.OmegaConf.set_struct(args, False)
     data_path = hydra.utils.to_absolute_path(args.data_path)
-    logger = logging.getLogger(__name__)
+    
     logger.info(args)
     PC_dataset = []
     try:
         for file in tqdm(os.listdir(data_path), total=len(os.listdir(data_path)), smoothing=0.9):
             if file.endswith(".mat"):
-                samples_PC = process(args, file)
+                log_memory_usage(logger)
+                samples_PC = process.process(args, file)
+                log_memory_usage(logger)
                 PC_dataset.extend(samples_PC)
-               
+              
         PT_args = load_PT_config(args.PT_config_path)
         TEST_PC, model = train_cls.main([PT_args, samples_PC])
         logger.info("Testing on dataset...")
@@ -148,6 +59,7 @@ def main(args):
             logger.info(f"Fused: F1 score: {F1_scores}, accuracy: {acc}, balanced accuracy: {balanced_acc}")
         else: 
             logger.info("\n".join([f"Node {i}: F1 score: {F1_scores[i]}, accuracy: {acc[i]}, balanced accuracy: {balanced_acc[i]}" for i in range(len(F1_scores))]))
+        
                 
     except Exception as error:
         logger.exception(error)

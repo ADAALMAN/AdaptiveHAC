f604ccd54ecca087b0adfe4d2e2ca55ae976d2c6
ADAALMAN <Adaalman@outlook.com>
2024-05-05 01:52:17 +0200

    Another bugfix

--- AdaptiveHAC/pointTransformer/point_transformer.py
+++ AdaptiveHAC/pointTransformer/point_transformer.py
@@ -29,10 +29,7 @@ def test(args, model, fusion, TEST_PC):
         else:
             classifier = model.eval()
         
-        y_pred_all = []
-        y_true_all = []
         # classify segments
-        
         TEST_PC_ALL = []
         # for bigger batches -> faster processing
         if isinstance(TEST_PC[0], PointCloud):
@@ -81,49 +78,58 @@ def test(args, model, fusion, TEST_PC):
         per_mean_label_all  = [per_mean_label_temp[i*(nr_nodes):(i+1)*(nr_nodes)] for i in range(int(len(true)/nr_nodes))]  
         seg_length_all      = [seg_length_temp[i*(nr_nodes):(i+1)*(nr_nodes)] for i in range(int(len(true)/nr_nodes))]  
         
-        for y_pred, y_true in zip(pred_all, true_all):       
-            match fusion:
-                case "none":
-                    pred_choice = torch.stack(y_pred,dim=0).data.max(1)[1]
-                    pred_choice = np.asarray(pred_choice.cpu())[:, np.newaxis].T  
-                    y_pred_all.extend(pred_choice)  
-                case "softmax":
-                    pred_choice = torch.stack(y_pred,dim=0).sum(dim=0)
-                    pred_choice = pred_choice.data.max(0)[1]
-                    pred_choice = np.asarray(pred_choice.cpu()).T
-                    y_pred_all.append(pred_choice)
-                case "majvote":
-                    pred_choice = torch.stack(y_pred,dim=0).mode(dim=0)
-                    pred_choice = pred_choice.data.max(0)[1]
-                    pred_choice = np.asarray(pred_choice.cpu()).T
-                    y_pred_all.append(pred_choice)   
-            y_true_all.append(y_true[0].cpu())
-                    
-        y_pred_all = np.asarray(y_pred_all)
-        if y_pred_all.ndim == 1:
-            y_pred_all = y_pred_all[:,np.newaxis]
-        y_true_all = np.asarray(y_true_all)[:,np.newaxis]
-        np.save(os.path.join(args.experiment_folder +'test_pred.npy'), y_pred_all)
-        np.save(os.path.join(args.experiment_folder +'test_true.npy'), y_true_all)
-        np.save(os.path.join(args.experiment_folder +'sequence_names.npy'), np.asarray(sequence_name_all)[:,np.newaxis])
-        np.save(os.path.join(args.experiment_folder +'H_scores.npy'), np.asarray(H_score_all)[:,np.newaxis])
-        np.save(os.path.join(args.experiment_folder +'per_labels.npy'), np.asarray(per_labels_all))
-        np.save(os.path.join(args.experiment_folder +'per_mean_label.npy'), np.asarray(per_mean_label_all)[:,np.newaxis])
-        np.save(os.path.join(args.experiment_folder +'seg_length.npy'), np.asarray(seg_length_all)[:,np.newaxis])
-        
-        F1_scores = []
-        acc = []
-        balanced_acc = []
-        for i in range(y_pred_all.shape[1]):
-            F1_scores.append(metrics.f1_score(y_true=y_true_all, y_pred=y_pred_all[:,i][:,np.newaxis], average="macro"))
-            acc.append(metrics.accuracy_score(y_true=y_true_all, y_pred=y_pred_all[:,i][:,np.newaxis], normalize=True))
-            balanced_acc.append(metrics.balanced_accuracy_score(y_true=y_true_all, y_pred=y_pred_all[:,i][:,np.newaxis]))
-            plt.close("all")
-            metrics.ConfusionMatrixDisplay.from_predictions(y_true=y_true_all, y_pred=y_pred_all[:,i][:,np.newaxis], 
-                                                    labels=np.arange(1, len(activities)-1), xticks_rotation=90, display_labels=activities[1:-1], 
-                                                    cmap=plt.cm.Blues)
-            plt.title("Fused" if fusion != "none" else f"Node: {i}")
-            plt.savefig("conf_matrix_fused.jpg" if fusion != "none" else f"conf_matrix_node_{i}.jpg", bbox_inches='tight')
+        # do all fusion
+        for fusion in ["none", "softmax", "majvote"]:
+            y_pred_all = []
+            y_true_all = []
+            os.mkdir(os.path.join(args.experiment_folder + "/" + fusion))
+            for y_pred, y_true in zip(pred_all, true_all):       
+                match fusion:
+                    case "none":
+                        pred_choice = torch.stack(y_pred,dim=0).data.max(1)[1]
+                        pred_choice = np.asarray(pred_choice.cpu())[:, np.newaxis].T  
+                        y_pred_all.extend(pred_choice)  
+                    case "softmax":
+                        pred_choice = torch.stack(y_pred,dim=0).sum(dim=0)
+                        pred_choice = pred_choice.data.max(0)[1]
+                        pred_choice = np.asarray(pred_choice.cpu()).T
+                        y_pred_all.append(pred_choice)
+                    case "majvote":
+                        pred_choice = torch.stack(y_pred,dim=0).mode(dim=0)
+                        pred_choice = pred_choice[0].max(0)[1]
+                        pred_choice = np.asarray(pred_choice.cpu()).T
+                        y_pred_all.append(pred_choice)   
+                y_true_all.append(y_true[0].cpu())
+                        
+            y_pred_all = np.asarray(y_pred_all)
+            if y_pred_all.ndim == 1:
+                y_pred_all = y_pred_all[:,np.newaxis]
+            y_true_all = np.asarray(y_true_all)[:,np.newaxis]
+            np.save(os.path.join(args.experiment_folder + "/" + fusion + '/test_pred.npy'), y_pred_all)
+            np.save(os.path.join(args.experiment_folder + "/" + fusion + '/test_true.npy'), y_true_all)
+            np.save(os.path.join(args.experiment_folder + "/" + fusion + '/sequence_names.npy'), np.asarray(sequence_name_all)[:,np.newaxis])
+            np.save(os.path.join(args.experiment_folder + "/" + fusion + '/H_scores.npy'), np.asarray(H_score_all)[:,np.newaxis])
+            np.save(os.path.join(args.experiment_folder + "/" + fusion + '/per_labels.npy'), np.asarray(per_labels_all))
+            np.save(os.path.join(args.experiment_folder + "/" + fusion + '/per_mean_label.npy'), np.asarray(per_mean_label_all)[:,np.newaxis])
+            np.save(os.path.join(args.experiment_folder + "/" + fusion + '/seg_length.npy'), np.asarray(seg_length_all)[:,np.newaxis])
+            
+            F1_scores = []
+            acc = []
+            balanced_acc = []
+            for i in range(y_pred_all.shape[1]):
+                F1_scores.append(metrics.f1_score(y_true=y_true_all, y_pred=y_pred_all[:,i][:,np.newaxis], average="macro"))
+                acc.append(metrics.accuracy_score(y_true=y_true_all, y_pred=y_pred_all[:,i][:,np.newaxis], normalize=True))
+                balanced_acc.append(metrics.balanced_accuracy_score(y_true=y_true_all, y_pred=y_pred_all[:,i][:,np.newaxis]))
+                plt.close("all")
+                metrics.ConfusionMatrixDisplay.from_predictions(y_true=y_true_all, y_pred=y_pred_all[:,i][:,np.newaxis], 
+                                                        labels=np.arange(1, len(activities)-1), xticks_rotation=90, display_labels=activities[1:-1], 
+                                                        cmap=plt.cm.Blues)
+                plt.title("Fused" if fusion != "none" else f"Node: {i}")
+                plt.savefig(os.path.join(args.experiment_folder + "/" + fusion + "/conf_matrix_fused.jpg") if fusion != "none" 
+                            else os.path.join(args.experiment_folder + "/" + fusion + f"/conf_matrix_node_{i}.jpg"), bbox_inches='tight')
+            np.savetxt(os.path.join(args.experiment_folder + "/" + fusion + '/F1.txt'), np.asarray(F1_scores), fmt='%1.5f', delimiter=',', newline='\n')
+            np.savetxt(os.path.join(args.experiment_folder + "/" + fusion + '/Accuracy.txt'), np.asarray(acc), fmt='%1.5f', delimiter=',', newline='\n')
+            np.savetxt(os.path.join(args.experiment_folder + "/" + fusion + '/Balanced_accuracy.txt'), np.asarray(balanced_acc), fmt='%1.5f', delimiter=',', newline='\n')
     torch.cuda.empty_cache()
     return F1_scores, acc, balanced_acc
 
